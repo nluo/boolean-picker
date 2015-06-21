f562c3f9532e2639398a11569b956652628dd43d
Nick Luo <nick.luo@me.com>
2015-06-22 01:11:19 +1000

    Updated example

--- test.js
+++ test.js
@@ -1,72 +1,21 @@
 var booleanPicker = require('./')
-var property = {
-  smoking: 'yes',
-  alarm: 'true',
-  features: ['hotWater', 'pool', {airCon: '1', pets: 'no'}],
-  features22: [{airCon: '1', pets: 'no'}, {airCon: '1', pets: 'no'}],
-  bedrooms: '3',
-  bathrooms: '1',
-  allowances: {
-  	thing: 'yes',
-  	bar: 'false'
-  }
-};
- 
-console.log(
-	booleanPicker(
-		property, 
-		{
-			smoking: true, 
-			alarm: true,
-			features: [{
-				pets: true
-			}],
-			features22: {
-				1: {
-					pets: true
-				}
-			},
-			allowances: {
-				thing: true
-			}
-		}, 
-		{
-			trues: ['yes', 'true'], 
-			falses: ['no', 'false']
-		}
-	)
-);
-
-
-// var booleanPicker = require('boolean-picker');
 var object = {
     foo: 'yes',
     bar: 'no',
     baz: {majigger: 'true'},
     animals: [{name: 'dog', isAlive: 'yes'}, {name: 'fish', isAlive: 'yes'}],
+    zed: 'affirmative'
 };
 
 var boolMap = {
   foo: true,
+  zed: true,
   baz: {
-  	majigger: true
+    majigger: true
   } 
 };
 
+var result = booleanPicker(object, boolMap, {trues: ['true', 'yes', 'affirmative'], falses: ['false', 'no']});
 
-var result = booleanPicker(object, boolMap, {trues: ['true', 'yes'], falses: ['false', 'no']});
 
 console.log(result);
-
-// booleanPicker = new BooleanPicker();
-
-// property = {
-//   smoking: 'yes',
-//   alarm: 'true',
-//   features: ['hotWater', 'pool', {airCon: '1', pets: 'no'}],
-//   bedrooms: '3',
-//   bathrooms: '1'
-// };
-
-
-// console.log(booleanPicker(property));
